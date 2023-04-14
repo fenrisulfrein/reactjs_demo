53f70de497c57979d2464405c369c96304af77a2
Yaroslav <yaroslav.basalyk@gmail.com>
2023-04-14 19:27:52 +0300

    add index.html

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const arr = [4, 4, 15];
-console.log(arr);
+const numbersOfFilms = prompt('Сколько фильмов вы уже посомотрели?', '');
+
+const personalMovieDB = {
+	count: numbersOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const nameOfFilm = prompt('Один из последних просмотренных фильмов', '');
+const rateOfFilm = prompt('Насколько оцените фильм?', '');
+const nameOfFilm2 = prompt('Один из последних просмотренных фильмов', '');
+const rateOfFilm2 = prompt('Насколько оцените фильм?', '');
+
+personalMovieDB.movies[nameOfFilm] = rateOfFilm;
+personalMovieDB.movies[nameOfFilm2] = rateOfFilm2;
+
+console.log(personalMovieDB);
